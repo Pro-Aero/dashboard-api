db568d1209c79ebea3eb680560074c6c59e52cd2
Rodrigo Lopes Marques <102917955+RodrigoLMarques@users.noreply.github.com>
2024-09-06 13:10:17 -0300

    feat: env module with zod

---
+++ src/modules/env/env.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+export const envSchema = z.object({
+  PORT: z.coerce.number().optional().default(3000),
+  MICROSOFT_CLIENT_ID: z.coerce.string(),
+  MICROSOFT_CLIENT_SECRET: z.coerce.string(),
+  MICROSOFT_TENANT_ID: z.coerce.string(),
+});
+
+export type Env = z.infer<typeof envSchema>;
